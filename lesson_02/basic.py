06f81a2b9d2822366f07156b29bb682cebe22e91
FemiAlabi <alabifemilateef@gmail.com>
2025-03-17 19:43:51 +0100

    lesson_02

---
+++ lesson_02/basic.py
@@ -0,0 +1,6 @@
+print ("Welcome to Python Code")
+x = 10
+y = 3
+if x > y:
+    print ("X is higher")
+
---
+++ lesson_02/basic02.py
@@ -0,0 +1,13 @@
+# total_score = 10
+# user_score = 5
+# print (total_score == user_score)
+# counter = 0
+# while True :
+#     if counter >= 10:
+#         break
+#     print (f"The count is now {counter}")
+# fruits = ["pen", "biro"]
+# for pen in fruits:
+#     print (pen)
+n = "latman" 
+print (n.upper())
